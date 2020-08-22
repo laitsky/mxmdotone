e97ec8cd784a6d58ecf258341eefbb2caa113956
laitsky <vincent.diamond15@gmail.com>
2020-08-22 16:19:57 +0700

    prettier

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const morgan = require("morgan");
-const config = require("./utils/config");
-const middleware = require("./utils/middleware");
-const Shortener = require("./models/shortener");
-const shortenersRouter = require("./controller/shorteners");
-const logger = require("./utils/logger");
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+const config = require('./utils/config');
+const middleware = require('./utils/middleware');
+const Shortener = require('./models/shortener');
+const shortenersRouter = require('./controller/shorteners');
+const logger = require('./utils/logger');
 
 const app = express();
 
-logger.info("connecting to", config.MONGODB_URI);
+logger.info('connecting to', config.MONGODB_URI);
 
 mongoose
   .connect(config.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => logger.info("connected to MongoDB"))
+  .then(() => logger.info('connected to MongoDB'))
   .catch((err) => logger.error(err.message));
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
-app.get("/", (req, res) => {
-  res.redirect("https://maxima.umn.ac.id");
+app.get('/', (req, res) => {
+  res.redirect('https://maxima.umn.ac.id');
 });
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.get("/twibbon", (req, res) => {
+app.get('/twibbon', (req, res) => {
   res.sendFile(`${__dirname}/public/index.html`);
 });
 
-app.get("/:shortUrl", async (req, res) => {
+app.get('/:shortUrl', async (req, res) => {
   const { shortUrl } = req.params;
   const result = await Shortener.findOne({ short_url: shortUrl });
   if (result) {
@@ -44,7 +44,7 @@ app.get("/:shortUrl", async (req, res) => {
   }
 });
 /* router placed below */
-app.use("/api/shorteners", shortenersRouter);
+app.use('/api/shorteners', shortenersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
